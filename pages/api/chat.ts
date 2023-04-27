5cdc6ed6918a4db1a25818b5ae5d0fe4bed74f3d
deezed420 <arjunrules55@gmail.com>
2023-04-26 21:46:32 -0400

    e

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -5,14 +5,15 @@ async function chatHandler(
   res: NextApiResponse
 )
 {const request = new XMLHttpRequest();
-request.open('POST', 'https://gptcloud.enderdabender.repl.co/v1/chat/completions', true);
+request.open('POST', 'https://gptcloud.enderdabender.repl.co/chat/completions', true);
 request.setRequestHeader('Authorization', 'Bearer pk-yvKRjKhttFJPgeeysUsfIJCKfgpXgUFTuFXqaHbssudOdeHC');
 request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 request.send(JSON.stringify({
     "model": "gpt-3.5-turbo",
     "messages": [
       {"role": "system", "content": "You are a helpful assistant named GPTCloud based on OpenAI ChatGPT"},
-    ].concat(req.body.messages)
+    ].concat(req.body.messages),
+    "temperature": 0
 }));}
 
 export default chatHandler;
